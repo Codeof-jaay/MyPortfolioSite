bd8af6160d4001d6ea18e7dfe4fa4fe20a8dce56
Codeof-jaay <anyanwujude435@gmail.com>
2025-10-05 17:02:24 +0100

    further progress

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -4,7 +4,11 @@ const Projects = () => {
   return (
     <section className="relative c-space section-spacing">
         <h2 className="text-heading">Projects</h2>
-    
+        <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent mt-12 h-[1px] w-full " />
+       { myProjects.map((project) => (
+        <Projects className="flex-wrap" key={project.id} />
+       ))}
+
     </section>
   )
 }
